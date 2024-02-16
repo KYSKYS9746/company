ab7ba1c9718594472e4a7c98ae82393cbc80c908
KYSKYS9746 <148739304+KYSKYS9746@users.noreply.github.com>
2024-02-16 18:11:27 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,44 @@
-$ (function(){
+$(function () {
 
-    
+
 
     $('.mopen').on('click', function () {
         $('.gnb').toggleClass('on');
         $(this).toggleClass('on');
     })
 
-    window.addEventListener('scroll', ()=> {
+    window.addEventListener('scroll', () => {
         let SCT = window.scrollY;
-        SCT > 0 
-        ? document.querySelector('.header').classList.add('on')
-        : document.querySelector('.header').classList.remove('on');
-    
+        SCT > 0
+            ? document.querySelector('.header').classList.add('on')
+            : document.querySelector('.header').classList.remove('on');
+
     });
-    
-        AOS.init();
+
+    AOS.init();
+
+    $('.totop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 800)
+    });
+
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 800) {
+            $('.totop').addClass('on');
+            $('.header').addClass('on');
+        } else {
+            $('.totop').removeClass('on');
+            $('.header').removeClass('on');
+        }
+    });
+
+    //-----숫자카운트-----
+    function countDi() {
+        $('.count_number').counterUp({
+            delay: 10,
+            time: 800,
+        });
+    }
+
+    countDi()
 })
